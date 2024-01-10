72aa9f700215ea9dd07451a3bcf948256acd5e52
ExtremeXT <75576145+ExtremeXT@users.noreply.github.com>
2024-01-10 18:55:32 +0200

    Fix current TCP congestion algorithm fetch

--- app/src/main/java/com/hades/hKtweaks/utils/kernel/misc/Misc.java
+++ app/src/main/java/com/hades/hKtweaks/utils/kernel/misc/Misc.java
@@ -39,6 +39,7 @@ public class Misc {
     private static final String GENTLE_FAIR_SLEEPERS = "/sys/kernel/sched/gentle_fair_sleepers";
     private static final String ARCH_POWER = "/sys/kernel/sched/arch_power";
     private static final String TCP_AVAILABLE_CONGESTIONS = "/proc/sys/net/ipv4/tcp_available_congestion_control";
+    private static final String TCP_ENABLED_CONGESTION = "/proc/sys/net/ipv4/tcp_congestion_control";
     private static final String HOSTNAME_KEY = "net.hostname";
     private static final String WIREGUARD = "/sys/module/wireguard/version";
     private static final String MAGISK_BIN = "/res/magisk";
@@ -126,7 +127,7 @@ public class Misc {
     }
 
     public String getTcpCongestion() {
-        return getTcpAvailableCongestions().get(0);
+        return Utils.readFile(TCP_CURRENT_CONGESTION);
     }
 
     public List<String> getTcpAvailableCongestions() {
